7378844599f191ae97c1782d8e84d2eeb13febb8
akshsharma1218 <akshsharma1218@gmail.com>
2022-09-23 13:34:45 +0530

    filtering and readme added

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('review_app.urls')),
+    path('',include('review_app.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- review_app/views.py
+++ review_app/views.py
@@ -1,12 +1,14 @@
 from .serializers import ApplicantSerializer
 from .models import Applicant
-
+from rest_framework import filters
 from rest_framework import status
 from rest_framework import generics
 
 class ApplicantListCreateView(generics.ListCreateAPIView):
     queryset = Applicant.objects.all()
     serializer_class = ApplicantSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['f_name','m_name','l_name']
 
 class ApplicantUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Applicant.objects.all()
